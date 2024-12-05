1c68e5eedca9db7a0415556558921a8a777ad77f
geovane833 <54215976+geovane833@users.noreply.github.com>
2024-12-04 22:28:37 -0300

    Add files via upload

---
+++ backend/Tapiocas/src/main/kotlin/com/geolanches/Tapiocas/TapiocasApplication.kt
@@ -0,0 +1,11 @@
+package com.geolanches.Tapiocas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TapiocasApplication
+
+fun main(args: Array<String>) {
+	runApplication<TapiocasApplication>(*args)
+}
